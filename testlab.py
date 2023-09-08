dc109bed7dd163478c5b42beed5575f449945e43
Vladislav <WWolkoW04@yandex.ru>
2023-09-08 18:15:26 +0300

    Загружается только 30 изображений и созданы папки

--- testlab.py
+++ testlab.py
@@ -1,11 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
-import re
-from re import sub
-from decimal import Decimal
-import io
-from datetime import datetime
-import pandas as pd
-import os
+import os #для создания папок
+import cv2
 
 
+URL = "https://yandex.ru/images/search?text=polarbear"
+html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"}, timeout = 3)
+soup = BeautifulSoup(html_page.text, "html.parser")#or html_page.content?
+img_links = soup.find_all('img', class_ = 'serp-item__thumb justifier__thumb')
+number = 0
+
+if not os.path.isdir('dataset'):
+    os.mkdir('dataset')
+
+os.chdir('dataset') # либо makedirs и рекурсивно создать папки
+
+if not os.path.isdir('polar_bear'):
+    os.mkdir('polar_bear')
+
+if not os.path.isdir('brown_bear'):
+    os.mkdir('brown_bear')
+'''
+for img_link in img_links:
+    try:
+        link = img_link.get('src')
+        img = requests.get('https:' + str(link))
+        with open(str(number) + '.jpg', 'wb') as f:
+            f.write(img.content)
+    except:
+        continue
+    number+=1
+'''
