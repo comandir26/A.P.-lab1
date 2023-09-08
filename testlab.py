b0198ed09b4b884c6cde2c5d193a93f3afeee1b4
Vladislav <WWolkoW04@yandex.ru>
2023-09-08 19:58:59 +0300

    Разбил код на функции. Загружается только по 30 фотографий для каждого класса

--- testlab.py
+++ testlab.py
@@ -3,31 +3,49 @@ import requests
 import os #для создания папок
 import cv2
 
-
-URL = "https://yandex.ru/images/search?text=polarbear"
-html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"}, timeout = 3)
-soup = BeautifulSoup(html_page.text, "html.parser")#or html_page.content?
-img_links = soup.find_all('img', class_ = 'serp-item__thumb justifier__thumb')
-number = 0
-
-if not os.path.isdir('dataset'):
-    os.mkdir('dataset')
-
-os.chdir('dataset') # либо makedirs и рекурсивно создать папки
-
-if not os.path.isdir('polar_bear'):
-    os.mkdir('polar_bear')
-
-if not os.path.isdir('brown_bear'):
-    os.mkdir('brown_bear')
-'''
-for img_link in img_links:
+def send_requests(url, name):
+    ''''
     try:
-        link = img_link.get('src')
-        img = requests.get('https:' + str(link))
-        with open(str(number) + '.jpg', 'wb') as f:
-            f.write(img.content)
-    except:
-        continue
-    number+=1
-'''
+        html_page = requests.get(str(url) + name, headers={"User-Agent":"Mozilla/5.0"}, timeout = 3) # 3 seconds
+    except requests.exceptions.Timeout:
+        print("Timed out")
+    '''
+    html_page = requests.get(str(url) + name, headers={"User-Agent":"Mozilla/5.0"}, timeout = 3)
+    soup = BeautifulSoup(html_page.text, "html.parser")
+    img_links = soup.find_all('img', class_ = 'serp-item__thumb justifier__thumb')
+    return img_links
+
+def make_folders(names):
+    if not os.path.isdir('dataset'):
+        os.mkdir('dataset')
+    if not os.path.isdir('dataset'):
+        os.mkdir('dataset')
+    os.chdir('dataset') #либо makedirs и рекурсивно создать папки
+    if not os.path.isdir(names[0]):
+        os.mkdir(names[0])
+    if not os.path.isdir(names[1]):
+        os.mkdir(names[1])
+
+def load_images(links, filename):
+    os.chdir(filename)
+    number = 0
+    for link in links:
+        try:
+            link = link.get('src')
+            img = requests.get('https:' + str(link))
+            with open(str(number).zfill(4) + '.jpg', 'wb') as f:
+                f.write(img.content)
+        except:
+            continue
+        number+=1
+    os.chdir('..')
+    print(os.getcwd())
+
+url = "https://yandex.ru/images/search?text="
+make_folders(('polar_bear', 'brown_bear'))
+
+polarbear_links = send_requests(url, 'polarbear')
+load_images(polarbear_links, 'polar_bear')
+
+brownbear_links = send_requests(url, 'brownbear')
+load_images(brownbear_links, 'brown_bear')
