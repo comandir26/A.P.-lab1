d0281794efc9f958c67ea2c2e9b321ae69ceefdf
Vladislav <WWolkoW04@yandex.ru>
2023-09-14 11:04:13 +0300

    Add page in URL

--- testlab.py
+++ testlab.py
@@ -1,26 +1,28 @@
+import os
+import shutil
+import time
+import random
 from bs4 import BeautifulSoup
 import requests
-import os #для создания папок
 import cv2
 
 def send_requests(url, name):
-    ''''
-    try:
-        html_page = requests.get(str(url) + name, headers={"User-Agent":"Mozilla/5.0"}, timeout = 3) # 3 seconds
-    except requests.exceptions.Timeout:
-        print("Timed out")
-    '''
-    html_page = requests.get(str(url) + name, headers={"User-Agent":"Mozilla/5.0"}, timeout = 3)
-    soup = BeautifulSoup(html_page.text, "html.parser")
-    img_links = soup.find_all('img', class_ = 'serp-item__thumb justifier__thumb')
-    return img_links
+    result = []
+    for page in range(1, 10):
+        html_page = requests.get(str(url) + name + "?page=" + str(page), headers={"User-Agent":"Mozilla/5.0"})
+        soup = BeautifulSoup(html_page.text, "html.parser")
+        img_links = soup.find_all('img', class_ = 'serp-item__thumb justifier__thumb')
+        result += img_links
+        print(len(result))
+        time.sleep(5)
+    return result
 
 def make_folders(names):
     if not os.path.isdir('dataset'):
         os.mkdir('dataset')
     if not os.path.isdir('dataset'):
         os.mkdir('dataset')
-    os.chdir('dataset') #либо makedirs и рекурсивно создать папки
+    os.chdir('dataset')
     if not os.path.isdir(names[0]):
         os.mkdir(names[0])
     if not os.path.isdir(names[1]):
@@ -41,11 +43,18 @@ def load_images(links, filename):
     os.chdir('..')
     print(os.getcwd())
 
+if os.path.isdir("dataset/polar_bear") and os.path.isdir("dataset/brown_bear"):
+    shutil.rmtree("dataset/polar_bear") 
+    shutil.rmtree("dataset/brown_bear")    
+
 url = "https://yandex.ru/images/search?text="
+
 make_folders(('polar_bear', 'brown_bear'))
 
+time.sleep(7)
 polarbear_links = send_requests(url, 'polarbear')
 load_images(polarbear_links, 'polar_bear')
 
+time.sleep(7)
 brownbear_links = send_requests(url, 'brownbear')
 load_images(brownbear_links, 'brown_bear')
