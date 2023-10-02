e2c2d84080d96fe294fc60b1efe068cc91ae75a1
Vladislav <WWolkoW04@yandex.ru>
2023-10-02 20:35:11 +0300

    Full-size images are loaded

--- testlab.py
+++ testlab.py
@@ -5,28 +5,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common import exceptions 
 import requests
+from bs4 import BeautifulSoup
 import cv2
 
-def send_requests(url, name):
+def send_requests(url, name, html_page):
     result = []
     page = 1
-    '''
     while True:
-        html_page = requests.get(url + name + "?page=" + str(page), headers={'User-Agent':'Mozilla/5.0'})
-        soup = BeautifulSoup(html_page.text, "html.parser")
-        img_links = soup.find_all("a", class_ = "serp-item__link")     
-        if len(img_links) > 0:
-            page+=1
-        result += img_links
-        print(len(result)) 
-        time.sleep(5) 
-        if len(result) > 0:
-            break
-    '''  
-    html_page = webdriver.Edge()
-    while True:
+        full_url = url + name #+ "?p=" + str(page)
         try: 
-            html_page.get(url + name)#+ "?p=" + str(page)  
+            html_page.get(full_url)  
             html_page.find_element(By.CLASS_NAME, "CheckboxCaptcha-Anchor").click()
         except exceptions.NoSuchElementException:
             print('No Captcha') 
@@ -35,10 +23,10 @@ def send_requests(url, name):
             page+=1
         result += img_links
         print(len(result)) 
-        time.sleep(5) 
+        time.sleep(2) 
         if len(result) > 0:
-            break
-        html_page.quit()
+            #html_page.quit()
+            break   
     return result
 
 def make_folders(names):
@@ -52,26 +40,27 @@ def make_folders(names):
 
 def load_images(links, filename):
     os.chdir(filename)
-    for number, link in enumerate(links):
-        #image_page = webdriver.Edge()
+    img_page = webdriver.Edge()
+    for number, link in enumerate(links): 
+        time.sleep(3)
         href = link.get_attribute('href')
-        #response = requests.get(href)
-        response = None    
-        while response is None:    
-            try:
-                response = requests.get(href, timeout=3)
-                break
-            except requests.exceptions.ConnectionError as e: 
-                print(e)
-                time.sleep(5)
-                continue
-        
+        img_page.get(href)
+        time.sleep(2)
+        try:
+            #img_origin = img_page.find_element(By.CLASS_NAME, "MMImage-Origin")
+            img_origin = img_page.find_element(By.CLASS_NAME, "MMImage-Origin")
+        except exceptions.NoSuchElementException:
+            print("No such element")
+            continue
+        img_link = img_origin.get_attribute('src')
+        response = requests.get(img_link, timeout=3)
+        time.sleep(2)
         with open(str(number).zfill(4) + '.jpg', 'wb') as f:
             f.write(response.content)
             print('Success')
-        time.sleep(2)
-
+        
     os.chdir('..')
+    img_page.close()
     print(os.getcwd())
 
 
@@ -80,16 +69,22 @@ url = "https://yandex.ru/images/search?text="
 class1 = "polarbear"
 class2 = "brownbear"
 
+'''
 if os.path.isdir("dataset/" + class1) and os.path.isdir("dataset/" + class2):
    shutil.rmtree("dataset/" + class1) 
-   shutil.rmtree("dataset/" + class2)    
+   shutil.rmtree("dataset/" + class2)
+'''   
 
 make_folders((class1, class2))
 
-polarbear_links = send_requests(url, class1)
+html_page = webdriver.Edge()
+
+polarbear_links = send_requests(url, class1, html_page)
 load_images(polarbear_links, class1)
 
-time.sleep(7)
+time.sleep(5)
 
-brownbear_links = send_requests(url, class2)
+brownbear_links = send_requests(url, class2, html_page)
 load_images(brownbear_links, class2)
+
+#html_page.close()
